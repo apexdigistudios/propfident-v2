0f982b25de199bf7e628f0568f2f31f227d8cf07
hello-arc <hello.missmave@gmail.com>
2026-09-18 01:07:13 +0000

    fix hero-bg

--- components/hero.tsx
+++ components/hero.tsx
@@ -23,20 +23,20 @@ export function Hero() {
 
   return (
     <section className="relative isolate overflow-hidden border-b border-border/40 pt-20 pb-16 md:pt-28 md:pb-24 bg-background">
-      {/* Background Image - Mobile View (hero-bg2.png) */}
-      <div className="block md:hidden absolute inset-0 -z-10 pointer-events-none">
+      {/* Mobile Background Image (hero-bg2.png) */}
+      <div className="md:hidden absolute inset-0 -z-10 pointer-events-none">
         <Image
           src="/hero-bg2.png"
           alt="Propfident Mobile Hero Background"
           fill
           priority
           sizes="100vw"
-          className="object-cover object-center opacity-90"
+          className="object-cover object-center"
         />
-        <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/60 to-background dark:from-background/40 dark:via-background/80 dark:to-background" />
+        <div className="absolute inset-0 bg-background/40" />
       </div>
 
-      {/* Background Image - Desktop View (hero-bg.png) */}
+      {/* Desktop Background Image (hero-bg.png) */}
       <div className="hidden md:block absolute inset-0 -z-10 pointer-events-none">
         <Image
           src="/hero-bg.png"
